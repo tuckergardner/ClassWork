9945d803629a70d14a95dc3c83f6796464c05ff3
whodattag@gmail.com <Lsutigerman1>
2018-02-27 09:03:02 -0600

    created instance variables, and compare to .

--- src/feb26/Fraction.java
+++ src/feb26/Fraction.java
@@ -1,6 +1,30 @@
 package feb26;
 
-public class Fraction
+public class Fraction implements Comparable <Fraction>
 {
+    private int num;
+     private int denom;
+     public Fraction (int num, int denom)
+     {
+         this.num = num;
+         this.denom = denom;
+         simplify();
+     }
+
+    @Override
+    public int compareTo(Fraction o)
+    {
+        int difference = this.num*o.denom - o.num*o.denom;
+        if (difference > 0) return 1;
+else if (difference < 0 ) return -1;
+else
+        return 0;
+    }
+
+
+    private void simplify()
+    {
+
+    }
 
 }

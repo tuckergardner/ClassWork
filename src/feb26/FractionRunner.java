ad1e694964646b95fcc46ed0d5a02fa9c8ef3e34
whodattag@gmail.com <Lsutigerman1>
2018-02-27 09:17:11 -0600

    tested out compare to method in runnder

--- src/feb26/FractionRunner.java
+++ src/feb26/FractionRunner.java
@@ -1,4 +1,11 @@
 package feb26;
 
-public class FractionRunner {
+public class FractionRunner
+{
+    public static void main (String[] args)
+    {
+        Fraction f1 = new Fraction(1,4);
+        Fraction f2 = new Fraction(1,3);
+        System.out.println(f1.compareTo(f2));
+    }
 }
